ad8a6b286eb5508c25f23e0a61a9fab1ca6ce112
Mouad <mouadse@gmail.com>
2024-11-23 21:41:23 +0100

    X

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,52 +1,29 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hsennane <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/19 04:10:14 by hsennane          #+#    #+#             */
-/*   Updated: 2024/11/19 12:48:59 by hsennane         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	size_t	i;
 	char	*d;
 	char	*s;
-	char	*lastd;
-	char	*lasts;
+	char	*end_dest;
+	char	*end_src;
 
-	i = 0;
 	d = (char *)dst;
 	s = (char *)src;
-	lastd = d + len - 1;
-	lasts = s + len - 1;
+	end_dest = d + len - 1;
+	end_src = s + len - 1;
 	if (!dst && !src)
 		return (NULL);
 	if (dst == src)
 		return (dst);
-	if (d < s)
+	if (dst < src)
 	{
-		while (i < len)
-		{
-			d[i] = s[i];
-			i++;
-		}
+		while (len--)
+			*d++ = *s++;
 	}
 	else
 	{
-		i = 0;
-		while (i < len)
-		{
-			*lastd = *lasts;
-			lastd--;
-			lasts--;
-			i++;
-		}
+		while (len--)
+			*end_dest-- = *end_src--;
 	}
 	return (dst);
 }
