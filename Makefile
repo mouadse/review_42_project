0ecbadca3fb939d70f0a9a60b9bc3c7f686c3629
Mouad <mouadse@gmail.com>
2024-11-23 22:22:45 +0100

    X

--- Makefile
+++ Makefile
@@ -1,11 +1,5 @@
-NAME    = libft.a
-CC      = cc
-CFLAGS  = -Wall -Wextra -Werror
-AR      = ar rcs
-RM      = rm -f
-
-# Source and Object Files
-CFILES  = ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c \
+NAME = libft.a
+CFILES = 	ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c \
 		ft_isprint.c ft_toupper.c ft_tolower.c ft_strlen.c ft_strchr.c \
 		ft_strrchr.c ft_strncmp.c ft_strnstr.c ft_strlcat.c ft_strlcpy.c \
 		ft_strjoin.c ft_bzero.c ft_memset.c ft_memcpy.c ft_memmove.c ft_memchr.c \
@@ -13,15 +7,18 @@ CFILES  = ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isdigit.c \
 		ft_strmapi.c ft_striteri.c ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c \
 		ft_putnbr_fd.c ft_atoi.c ft_split.c ft_strtrim.c ft_itoa.c
 
-OFILES  = $(CFILES:.c=.o)
+OFILES = $(CFILES:.c=.o)
+CFLAGS = -Wall -Wextra -Werror
+CC = cc
+RM = rm -f
+AR = ar rcs
 
-# Rules
 all: $(NAME)
 
 $(NAME): $(OFILES)
 	$(AR) $(NAME) $(OFILES)
 
-%.o: %.c
+.c.o:
 	$(CC) $(CFLAGS) -c $< -o $@
 
 clean:
@@ -32,4 +29,5 @@ fclean: clean
 
 re: fclean all
 
-.PHONY: all clean fclean re
+.PHONY: clean fclean re all
+
--- ft_strjoin.c
+++ ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hsennane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: hsennane <hsennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 12:50:00 by hsennane          #+#    #+#             */
-/*   Updated: 2024/11/19 13:00:26 by hsennane         ###   ########.fr       */
+/*   Updated: 2024/11/23 22:20:04 by hsennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,32 +20,21 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	index[2];
 
 	if (!s1 || !s2)
-	{
 		return (NULL);
-	}
 	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2); // Fixed: Use s2 instead of s1
+	s2_len = ft_strlen(s2);
 	p = malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (!p)
-	{
 		return (NULL);
-	}
-	// Initialize indices
-	index[0] = 0; // Index for s1 and p
-	index[1] = 0; // Index for s2
-	// Copy s1 into p
+	index[0] = 0;
+	index[1] = 0;
 	while (index[0] < s1_len)
 	{
 		p[index[0]] = s1[index[0]];
 		index[0]++;
 	}
-	// Copy s2 into p
 	while (index[1] < s2_len)
-	{
-		p[index[0]] = s2[index[1]];
-		index[0]++;
-		index[1]++;
-	}
-	p[index[0]] = '\0'; // Fixed: Null terminator at the end of p
+		p[index[0]++] = s2[index[1]++];
+	p[index[0]] = '\0';
 	return (p);
 }
