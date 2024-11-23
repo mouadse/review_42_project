94f32cfe98ae41d4f18a56ee4c08733810087c44
Mouad <mouadse@gmail.com>
2024-11-23 21:46:10 +0100

    x

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,13 +3,12 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hsennane <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: hsennane <hsennane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 12:06:42 by hsennane          #+#    #+#             */
-/*   Updated: 2024/11/16 13:56:40 by hsennane         ###   ########.fr       */
+/*   Updated: 2024/11/23 21:44:55 by hsennane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft.h"
 
 int	ft_atoi(const char *str)
 {
@@ -39,26 +38,3 @@ int	ft_atoi(const char *str)
 	}
 	return (sign * result);
 }
-
-// int ft_atoi(const char *str) {
-//   int i;
-//   int sign;
-//   int result;
-//
-//   i = 0;
-//   sign = 1;
-//   result = 0;
-//   while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ') {
-//     i++;
-//   }
-//   if (str[i] == '+' || str[i] == '-') {
-//     if (str[i] == '-')
-//       sign *= -1;
-//     i++;
-//   }
-//   while (str[i] >= '0' && str[i] <= '9') {
-//     result = result * 10 + str[i] - '0';
-//     i++;
-//   }
-//   return (result * sign);
-// }
